b6df52ecacac92e8c52c12332dd633024fd07f93
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-04-08 10:30:20 +0200

    ricorsiva del numero Fattoriale es_3 lezione 13

--- Python/Lezione_13_ricorsive/esercizi_funzioni_ricorsive/es_3_fattoriale.py
+++ Python/Lezione_13_ricorsive/esercizi_funzioni_ricorsive/es_3_fattoriale.py
@@ -7,6 +7,21 @@ n * (n-1) * (n-2) * ... * 1
 con 1! uguale a 1 e 0! definito come 1.
 Si scriva una funzione ricorsiva recursiveFactorial che dato un numero n calcoli n!.'''
 
-def recursive_factorial(n:int):
 
-    n
+''' se n = 1 o a 0 ci fermiamo'''
+
+''' 5! = 5 * 4 * 3 * 2 * 1 
+    5! = 5 * 4! = 5 * 4 * 3! = 5 * 4 * 3 * 2! = 5 * 4 * 3 * 2 * 1! = 5 * 4 * 3 * 2 * 1 = 120'''
+
+def recursive_factorial(n:int):
+    if n < 0:
+        print("Error! Inserire numero positivo")
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        return int(n * recursive_factorial(n -1))
+    
+risultato = recursive_factorial(5)
+print(risultato)
+
+print(f"5! = {recursive_factorial(5)}")
