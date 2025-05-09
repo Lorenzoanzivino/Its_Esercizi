dc240258f878de4f080f40c161744910aa64f0c6
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-05-09 11:22:19 +0200

    esercizi 1 e 2 lezione 15

--- Python/Lezione_15_File/example.py
+++ Python/Lezione_15_File/example.py
@@ -13,7 +13,26 @@ except Exception as e:
 finally :
     file.close()
 
-# CONTESTO
-# Uscendo dal print che è indentato il file si chiude automaticamente
+# CONTEXT : (context manager = classe che ha __enter__() / __exit__())
+# Uscendo dal print che è indentato il file si chiude automaticamente# il WITH sostituisce la creazione del percorso di un file scritto sopra
 with open("example.txt", "w") as file :
     print(file.read())
+
+
+# Spiegazione di come funziona il WITH gestisce tutto in automatico tramite questa classe
+class MyResource :
+    def __enter(self) :
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        print("Sono nella funzione")
+
+        if exc_type is not None:
+            print(f"An error occured: {exc_value}")
+
+        return True
+    
+print("iNIZIO DEL PROGRAMMA")
+
+with MyResource() as resources :
+    print("Sono dentro il blocco with")
