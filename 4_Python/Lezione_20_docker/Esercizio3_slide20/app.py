2732cec1f58d26a4a8d355676516cb2278416c02
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-05-20 10:53:34 +0200

    modifica appunti e esercizio 3 lezione 20 docker

---
+++ 4_Python/Lezione_20_docker/Esercizio3_slide20/app.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def main():
+    numbers = np.array([10, 20, 30, 40, 50])
+    mean = np.mean(numbers)
+    print("Numeri:", numbers)
+    print("Media:", mean)
+
+if __name__ == "__main__":
+    main()
