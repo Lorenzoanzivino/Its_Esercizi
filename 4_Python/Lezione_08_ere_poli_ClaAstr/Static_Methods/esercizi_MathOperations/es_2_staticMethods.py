83f979af41df116cd93125c19cac698d6cb4f7d4
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-05-21 09:41:04 +0200

    aggiunta traccia es 2 statich Methods

---
+++ 4_Python/Lezione_08_ere_poli_ClaAstr/Static_Methods/esercizi_MathOperations/es_2_staticMethods.py
@@ -0,0 +1,9 @@
+'''Esercizio 2: Attuazione dei metodi statici
+
+Crea una classe chiamata MathOperations per raggruppare alcune funzionalità aritmetiche di base. All'interno di questa classe, definiamo due metodi statici:
+
+    Aggiungi, che accetta due parametri numerici e restituisce la loro somma.
+
+    Moltiplicare, che prende anche due parametri numerici e restituisce il loro prodotto.
+
+Infine, scrivere un piccolo programma di driver per testare la funzionalità dei metodi di aggiunta e moltiplicazione. Ciò dovrebbe comportare la chiamata sia con i metodi con gli input del campione che la stampa dei risultati per verificare che funzionino correttamente.'''
