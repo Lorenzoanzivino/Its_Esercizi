c4a3c00d94c5531a271e8ec6cb70541e37d697d7
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-05-23 10:50:28 +0200

    timeRange progettazione voliaerei1

--- 3_Progettazione/2_VoliAerei1/progettazione/tipi di dato/timeRange.py
+++ 3_Progettazione/2_VoliAerei1/progettazione/tipi di dato/timeRange.py
@@ -1,61 +1,64 @@
-from datetime import datetime, timedelta
 from typing import Self, Any
+from datetime import *
 
-class TimeRange(tuple):
-    #Tipo dato composto Periodo
-    # rappresenta un periodo di tempo compreso tra due istanti
-
+class TimeRange(tuple):	
 	_start: datetime|None # [0..1]
 	_end: datetime|None # [0..1]
-       
 	# subject to constraint: _start <= _end
 
 	def __new__(cls, start:datetime|str|None, end:datetime|str|None)->Self:	
 		# We accept _start == None or _end == None (unbounded time periods)
-              
-        values = [ datetime.fromisoformat(x) if isinstance(x, str) else x for x in [start, end] ]
-        if values[0] and values[1] and values[0] > values[1]:
-            raise ValueError(f"Invalid TimeRange(from={start}, to={end}): 'from' must be <= 'to'")
-        
-        return tuple.__new__(cls, values)   
-              
-#    def __init__(self, start:datetime, end : datetime) -> None :
-#        if start > end:
-#            raise ValueError(f"Invalid TimeRange( strat = {start}, end={end}); 'start' must be <= 'end'")
-#        
-#        self._start = start
-#        self._end = end
 
-    def start(self) -> datetime:
-        return self._start
-    
-    def end(self) -> datetime:
-        return self._end
-    
-    def duration(self) -> timedelta:
-        return self.end() - self.start()
-    
-#    def __hash__(self) -> int:
-#        return hash((self.start(), self.end()))
-#    
-#    def __eq__(self, other: any) -> bool:
-#        if not isinstance(other, type(self)) or hash(self) != hash(other):
-#            return False
-#        return self.start() == other.start() and self.end() == other.end()
-    
-    def is_disjoint(self, other: Self) -> bool:
-        """
-        se self e other sono intervalli che non si sovrappongono restituisce True
-        
-                    start                   end
-                self: |----------------------|
-                
-        start     end
-        |----------|
-                                        start   end
-                                          |------|
-                                          
-        """
+		values = [ datetime.fromisoformat(x) if isinstance(x, str) else x for x in [start, end] ]
+		if values[0] and values[1] and values[0] > values[1]:
+			raise ValueError(f"Invalid TimeRange(from={start}, to={end}): 'from' must be <= 'to'")
+		return tuple.__new__(cls, values)
 
-        return self.start() > other.end() or self.end() < other.start()
+	def start(self)->datetime|None:
+		return self[0]
+	def end(self)->datetime|None:
+		return self[1]
 
+	def duration(self)->timedelta|None:
+		try:
+			return self.end() - self.start()
+		except:
+			return timedelta.max # we should return timedelta(+infty), but timedelta has no standard representation of +infty
+
+	def is_disjoint(self, other:Self)->bool:
+
+		# case #1:
+		# self :        X---------------
+		# other: ---X           
+
+		# case #2:
+		# self : ---------------X
+		# other:                   X----
+
+		# case #3:
+		# self :        X-------X
+		# other: ---X      or      X----
+
+		if self.start(): #1, #3
+			if other.end() and other.end() < self.start():
+				return True #1
+			
+		if self.end(): #2, #3
+			if other.start() and other.start() > self.end():
+				return True #3
+
+		return False
+
+	def intersects(self, other:Self)->bool:
+		return not self.is_disjoint(other)
+
+	def shift(self, delta:timedelta)->Self:
+		if not delta:
+			return self
+		else:
+			return TimeRange(
+				None if not self.start() else self.start() + delta, 
+				None if not self.end() else self.end() + delta)
+
+	def __str__(self)->str:
+		return f"{ f'[{self[0]}' if self[0] else '(-infinity'}, { f'{self[1]}]' if self[1] else '+infinity)'}"
