60a7bb5a78523d22300e9946bad06463c82afdcb
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-05-28 19:24:16 +0200

    modifica Lezione 14 pytest

--- 4_Python/Lezione_14_PyTest/my_project/tests/test_calculator.py
+++ 4_Python/Lezione_14_PyTest/my_project/tests/test_calculator.py
@@ -2,18 +2,18 @@ from my_project.calculator import Calculator
 import pytest
 @pytest.fixture
 
-def test_addition():
-    calculation: Calculator = Calculator(10, 5)
+def calculation():
+    # creates a fresh istance of calculator before each test
+    return Calculator(10, 5)
+
+def test_addition(calculation):
     assert calculation.addition() == 13, "The sum is wrong"
 
-def test_subtraction():
-    Calculation: Calculator = Calculator(10, 5)
-    assert Calculation.subtraction() == 5, "The substraction is wrong"
+def test_subtraction(calculation):
+    assert calculation.subtraction() == 5, "The substraction is wrong"
 
-def test_multiplication():
-    Calculation: Calculator = Calculator(10, 5)
-    assert Calculation.multiplication() == 50, "The multiplication is wrong"
+def test_multiplication(calculation):
+    assert calculation.multiplication() == 50, "The multiplication is wrong"
 
-def test_division():
-    Calculation: Calculator = Calculator(10, 5)
-    assert Calculation.division() == 2.00, "The quotient is wrong"
+def test_division(calculation):
+    assert calculation.division() == 2.00, "The quotient is wrong"
