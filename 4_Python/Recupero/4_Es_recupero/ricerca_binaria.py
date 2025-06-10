a68cb49db286ee4361dd9eec729ac5d06a086fb0
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-06-10 19:03:07 +0200

    ricerca binaria

--- 4_Python/Recupero/4_Es_recupero/ricerca_binaria.py
+++ 4_Python/Recupero/4_Es_recupero/ricerca_binaria.py
@@ -28,3 +28,23 @@ valore_non_presente = 4
 
 print(ricerca_binaria(lista, valore_da_cercare))  # Output atteso: True
 print(ricerca_binaria(lista, valore_non_presente))  # Output atteso: False
+
+
+def bin_search(lista, numero) -> None:
+
+    mid:int = len(lista) // 2
+
+    if lista[mid] == numero:
+        print("Il numero è stato trovato")
+        # return None
+    
+    elif len(lista) == 1:
+        print("Il numero non c'è!")
+    
+    elif lista[mid] > numero: #il numero che sto cercando è a sinistra
+        bin_search(lista[:mid], numero)
+
+    elif lista[mid] < numero: #il numero che sto cercando è a destra
+        bin_search(lista[mid + 1:], numero)
+
+    
