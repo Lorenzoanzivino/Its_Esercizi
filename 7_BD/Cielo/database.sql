ac0f3b96590524f4b7c97b6c4cc74061029e1443
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-07-22 14:16:28 +0200

    Cielo DB

---
+++ 7_BD/Cielo/database.sql
@@ -0,0 +1,52 @@
+begin transaction;
+
+CREATE DOMAIN PosInteger AS Integer
+    check (value >= 0);
+
+CREATE DOMAIN StringaM AS VARCHAR(100);
+
+CREATE DOMAIN CodIATA AS CHAR(3);
+
+create table Compagnia(
+    nome StringaM primary key,
+    annoFondaz PosInteger
+);
+
+create table Volo(
+    codice PosInteger,
+    comp StringaM,
+    durataMinuti PosInteger not null,
+    primary key (codice, comp),
+
+    foreign key (comp) references Compagnia(nome)
+);
+
+create table Aeroporto(
+    codice CodIATA primary key, 
+    nome StringaM not null
+);
+
+create table LuogoAeroporto(
+    aeroporto CodIATA primary key, 
+    citta StringaM not null, 
+    nazione StringaM not null, 
+    foreign key (aeroporto) 
+        references Aeroporto(codice) deferrable
+);
+
+create table ArrPart(
+    codice PosInteger, 
+    comp StringaM , 
+    arrivo CodIATA not null,
+    partenza CodIATA not null,
+    primary key (codice, comp),
+
+    foreign key (codice, comp) references Volo(codice, comp) deferrable,
+    foreign key (arrivo) references Aeroporto(codice),
+    foreign key (partenza) references Aeroporto(codice)
+);
+
+alter table volo add foreign key (codice, comp) references ArrPart(codice, comp) deferrable;
+alter table aeroporto add foreign key (codice) references LuogoAeroporto(aeroporto) deferrable;
+
+commit;
--- 7_BD/accademia_query.sql
+++ 7_BD/accademia_query.sql
@@ -1,31 +1,33 @@
 -- 1. Quali sono il nome, la data di inizio e la data di fine dei WP del progetto di nome ‘Pegasus’ ?
 
-select wp.nome, wp.inizio, wp.fine 
-from wp, progetto 
-where progetto.nome = 'Pegasus' and wp.progetto = progetto.id;
+SELECT wp.nome, wp.inizio, wp.fine
+FROM progetto p, wp 
+WHERE p.id = wp.progetto
+	AND p.nome = 'Pegasus';
+
 
 -- 2. Quali sono il nome, il cognome e la posizione degli strutturati che hanno almeno
 -- una attività nel progetto ‘Pegasus’, ordinati per cognome decrescente?
 
-SELECT distinct Persona.nome, Persona.cognome, Persona.posizione
-FROM Persona, AttivitaProgetto, Progetto
-WHERE Persona.id = AttivitaProgetto.persona
-  AND AttivitaProgetto.progetto = Progetto.id
-  AND Progetto.nome = 'Pegasus'
-order by Persona.cognome desc;
+SELECT distinct p.id, p.nome, p.cognome, p.posizione
+FROM Persona p,  Progetto prog, AttivitaProgetto ap
+WHERE p.id = ap.persona
+  AND prog.nome = 'Pegasus'
+  AND prog.id = ap.progetto
+order by p.cognome desc;
 
 
 -- 3. Quali sono il nome, il cognome e la posizione degli strutturati che hanno più di
 -- una attività nel progetto ‘Pegasus’ ?
 
-select distinct p.nome, p.cognome, p.posizione
-from persona p, attivitaprogetto ap, attivitaprogetto ap2,progetto pr
-where p.id = ap.persona 
-	and ap.progetto = pr.id 
-	and ap2.id <> ap.id
-	and ap2.persona = ap.persona
-	and ap2.progetto = ap.progetto
-	and pr.nome ='Pegasus'
+select distinct p.id, p.nome, p.cognome, p.posizione
+from persona p, attivitaprogetto ap1, attivitaprogetto ap2, progetto prog
+where prog.nome = 'Pegasus'
+	and ap1.persona = p.id
+	and ap2.persona = p.id
+	and ap1.progetto = prog.id
+	and ap2.progetto = prog.id
+	and ap1.id <> ap2.id -- <> = DIVERSO
 order by p.cognome desc;
 
 
@@ -76,7 +78,7 @@ where a1.persona = p.id
 -- 8. Quali sono il nome e il cognome degli strutturati che nello stesso giorno hanno sia
 -- attività progettuali che attività non progettuali?
 
-select p.nome, p.cognome
+select distinct p.nome, p.cognome
 from persona p, AttivitaProgetto ap, AttivitaNonProgettuale anp
 where ap.persona = p.id
 	and anp.persona = p.id
@@ -88,7 +90,7 @@ where ap.persona = p.id
 -- giorno, il nome del progetto, il tipo di attività non progettuali e la durata in ore di
 -- entrambe le attività.
 
-select p.nome , p.cognome, pr.nome as prj, ap.oreDurata as h_prj, anp.tipo as att_noprj , anp.oreDurata as h_noprj 
+select distinct p.nome , p.cognome, pr.nome as prj, ap.oreDurata as h_prj, anp.tipo as att_noprj , anp.oreDurata as h_noprj 
 from persona p, AttivitaProgetto ap, AttivitaNonProgettuale anp, progetto pr
 where ap.persona = p.id
 	and anp.persona = p.id
@@ -124,4 +126,11 @@ select distinct wp1.nome
 from wp wp1, wp wp2, progetto p
 where wp1.nome = wp2.nome
 	and wp1.progetto = p.id
-	and wp1.progetto <> wp2.progetto;
+	-- and wp1.progetto <> wp2.progetto
+	and wp1.progetto < wp2.progetto;
+
+
+-- iniziare con SELECT * FROM colonne che mi servono,
+-- rinominare le colonne ad esempio FROM persona p
+-- selezionare solo le colonne utili nel select
+-- WHERE quello che mi serve
