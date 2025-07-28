26c9acf204297dddc58f90f4dd39fa76d30b329a
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-07-28 12:36:19 +0200

    secondaApp Web-2 VITE

---
+++ 2_Web/Web_2/React/seconda_app/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import UserAlbums from './componenti/UserAlbums'
+
+function App() {
+  return (
+    <UserAlbums />
+  )
+}
+
+export default App
