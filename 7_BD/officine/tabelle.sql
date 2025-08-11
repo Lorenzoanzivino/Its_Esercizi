6299a3436f200b6147acc389b6b53cb8f5df4d57
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-08-11 15:50:11 +0200

    tabelle modificate sql officine

--- 7_BD/officine/tabelle.sql
+++ 7_BD/officine/tabelle.sql
@@ -7,31 +7,31 @@ CREATE TABLE Persona (
 );
 
 CREATE TABLE Officina (
-    id_officina intero PRIMARY KEY,
+    id_officina integer PRIMARY KEY,
     Nome stringa NOT NULL,
     Indirizzo stringa NOT NULL
 );
 
 CREATE TABLE Staff (
-    id_staff intero PRIMARY KEY,
+    id_staff integer PRIMARY KEY,
     codicefiscale_staff codicefiscale UNIQUE NOT NULL,
     FOREIGN KEY (codicefiscale_staff) REFERENCES Persona(codicefiscale)
 );
 
 CREATE TABLE Proprietario (
-    id_proprietario intero PRIMARY KEY,
+    id_proprietario integer PRIMARY KEY,
     codicefiscale_proprietario codicefiscale UNIQUE NOT NULL,
     FOREIGN KEY (codicefiscale_proprietario) REFERENCES Persona(codicefiscale)
 );
 
 CREATE TABLE Dipendente (
-    id_dipendente intero PRIMARY KEY,
-    anni_servizio intero NOT NULL,
+    id_dipendente integer PRIMARY KEY,
+    anni_servizio integer NOT NULL,
     FOREIGN KEY (id_dipendente) REFERENCES Staff(id_staff)
 );
 
 CREATE TABLE Direttore (
-    id_direttore intero PRIMARY KEY,
+    id_direttore integer PRIMARY KEY,
     DataNascita data_nascita NOT NULL,
     FOREIGN KEY (id_direttore) REFERENCES Staff(id_staff)
 );
@@ -40,22 +40,22 @@ CREATE TABLE Veicolo (
     targa VARCHAR(10) PRIMARY KEY,
     Modello stringa NOT NULL,
     Tipo stringa NOT NULL,
-    chilometri intero NOT NULL
+    chilometri integer NOT NULL
 );
 
 CREATE TABLE Riparazione (
-    id_riparazione intero PRIMARY KEY,
+    id_riparazione integer PRIMARY KEY,
     Codice stringa NOT NULL,
     DataAccettazione data_nascita NOT NULL,
     OraAccettazione orario NOT NULL,
     targa_veicolo VARCHAR(10) NOT NULL,
-    id_proprietario intero NOT NULL,
+    id_proprietario integer NOT NULL,
     FOREIGN KEY (targa_veicolo) REFERENCES Veicolo(targa),
     FOREIGN KEY (id_proprietario) REFERENCES Proprietario(id_proprietario)
 );
 
 CREATE TABLE RiparazioneTerminata (
-    id_riparazione intero PRIMARY KEY,
+    id_riparazione integer PRIMARY KEY,
     DataConsegna data_nascita NOT NULL,
     OraConsegna orario NOT NULL,
     targa_veicolo VARCHAR(10) NOT NULL,
@@ -64,8 +64,8 @@ CREATE TABLE RiparazioneTerminata (
 );
 
 CREATE TABLE LavoraPresso (
-    id_dipendente intero NOT NULL,
-    id_officina intero NOT NULL,
+    id_dipendente integer NOT NULL,
+    id_officina integer NOT NULL,
     inizio_servizio data_nascita NOT NULL,
     fine_servizio data_nascita,
     PRIMARY KEY (id_dipendente, id_officina),
