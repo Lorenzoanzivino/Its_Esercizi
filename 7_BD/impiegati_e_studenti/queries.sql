ab3b80394e63bc30b1f40172e5da9953080883c0
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-09-18 15:58:39 +0200

    dati e query impiegati e studenti

--- 7_BD/impiegati_e_studenti/queries.sql
+++ 7_BD/impiegati_e_studenti/queries.sql
@@ -1,56 +1,55 @@
 --(Riguardare le slide se necessario!)
 
---1. Quali sono i nomi degli impiegati nati a partire dall'anno 
---2. Quali sono i nomi di tutti i progetti?
---3. Quali sono gli stipendi dei direttori?
---4. Quanti sono i progettisti?
---5. Quanti sono i responsabili?
---6. Quanti sono i progettisti che non sono responsabili? Non la sapete fare!
---7. Qual è lo stipendio medio dei segretari?
---8. Qual è l'età della/o studente meno giovane?
-    usare select(date_part('year',age(current_date, <DATA DI NASCITA>))) as eta FROM [...];
---9. Quanti sono i direttori che hanno assolto agli obblighi militari?
---10. Quanti sono i progetti di cui è responsabile un'impiegata con almeno due figli?
-
 -- Consegnare un unico file queries.sql contenente il codice SQL.
 
-
--- 1
-select nome from persona, impiegato
-where persona.cf = impiegato.persona
+-- 1. Quali sono i nomi degli impiegati nati a partire dall'anno 1965
+select nome 
+from persona p, impiegato i
+where p.cf = i.persona
+    and data_nascita >= '1965-01-01'
 order by data_nascita asc;
 
--- 2
+-- 2. Quali sono i nomi di tutti i progetti?
 select nome from progetto;
 
--- 3
+-- 3. Quali sono gli stipendi dei direttori?
 select stipendio from impiegato
 where ruolo = 'Direttore';
 
--- 4
+-- 4. Quanti sono i progettisti?
 select count (*) from impiegato
 where ruolo = 'Progettista';
 
--- 5
-select count (*) from impiegato, progetto
+-- 5. Quanti sono i responsabili?
+select count (distinct resp_prog) from impiegato, progetto
 where ruolo = 'Progettista';
 
--- 7
+
+-- 6. Quanti sono i progettisti che non sono responsabili? Non la sapete fare!
+select * from impiegato
+where impiegato.ruolo = 'Progettista'
+    and persona not in (
+        select distinct resp_prog 
+        from progetto
+        );    -- una query in una query
+        
+
+-- 7. Qual è lo stipendio medio dei segretari?
 select AVG(stipendio) from impiegato
 where ruolo = 'Segretario';
 
--- 8
-select max(date_part('year', age(current_date, data_nascita))) as eta
+-- 8. Qual è l'età della/o studente meno giovane?
+select MAX(date_part('year', age(current_date, data_nascita))) as eta
 FROM persona, studente
 where persona.cf = studente.persona;
 
--- 9
+-- 9. Quanti sono i direttori che hanno assolto agli obblighi militari?
 select count(cf) from persona, impiegato
 where ruolo = 'Direttore' 
     and persona.cf = impiegato.persona 
-    and persona.pos_uomo is not null;
+    and persona.pos_uomo = 'Assolto';
 
--- 10
-select count(id) from persona, progetto
+-- 10. Quanti sono i progetti di cui è responsabile un'impiegata con almeno due figli?
+select count(*) from persona, progetto
 where persona.cf = progetto.resp_prog
 	and persona.maternita >=2;
